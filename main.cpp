5292ab9e819bfddc3f78f7601180f4ce2f614494
Kayzori <kayzori7@gmail.com>
2026-02-01 19:24:12 +0100

    main modified

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main()
     FZXLog::initialize();
     FZXLog::setPattern(FZXLOG_FMT_PATTERN_FULL);
     FZXLog::info(FZXLOG_LOCATION(__FUNCTION__), "FZXLog initialized successfully.");
-    FZXLog::success(FZXLOG_LOCATION(__FUNCTION__), "This is a success message with number: {}", 42);
     FZXLog::shutdown();
     return 0;
 }
